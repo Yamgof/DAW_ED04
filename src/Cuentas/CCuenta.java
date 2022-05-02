15eefc0b1f3c5a164bee848b672a174214b6da4f
Fabio Yamuza Gonzalez <fabio.yamuza@gmail.com>
2022-05-02 19:53:03 +0100

    Se ha refactorizado el codigo tanto de la clase Main como de la clase CCuenta(renombramiento de la variable miCuenta a cuenta1, creacion de metodo operativa_cuenta parametrizada para realizar operaciones de ingresar y retirar y encapsulamiento de las propiedades de la clase CCuenta).

--- src/Cuentas/CCuenta.java
+++ src/Cuentas/CCuenta.java
@@ -1,9 +1,11 @@
+package Cuentas;
+
 public class CCuenta {
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
     public CCuenta() {
     }
@@ -12,22 +14,6 @@ public class CCuenta {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
-    }
-
-    public void asignarNombre(String nom) {
-        nombre = nom;
-    }
-
-    public String obtenerNombre() {
-        return nombre;
-    }
-
-    public double estado() {
-        return saldo;
-    }
-	
-	public String obtenerCuenta() {
-        return cuenta;
     }
 
     public void ingresar(double cantidad) throws Exception {
@@ -40,8 +26,40 @@ public class CCuenta {
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
-        if (estado() < cantidad)
+        if (getSaldo() < cantidad)
             throw new Exception("No hay suficiente saldo");
         saldo = saldo - cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
     }
 }
