fa7cd74dd2e865bf90f43d273124e67fb5b228fc
Fabio Yamuza Gonzalez <fabio.yamuza@gmail.com>
2022-05-09 19:34:27 +0100

    se han añadido comentarios javadoc

--- src/Cuentas/CCuenta.java
+++ src/Cuentas/CCuenta.java
@@ -1,5 +1,11 @@
 package Cuentas;
 
+/**
+ * @author Fabio Yamuza Gonzalez
+ * clase CCuenta en la que estaran los metodos y los parametros para crear y usar una cuenta bancaria
+ * una excepción.
+ */
+
 public class CCuenta {
 
     private String nombre;
@@ -7,22 +13,37 @@ public class CCuenta {
     private double saldo;
     private double tipoInteres;
 
+
     public CCuenta() {
     }
 
+    /**
+     * @param nom le da un valor al atributo nombre
+     * @param cue le da un valor al atributo ceunta
+     * @param sal le da un valor al atributo saldo
+     * @param tipo le daria valor a tipoInteres en caso de que se llegara a implementar en el constructor
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
 
+    /**
+     * @param cantidad se mete la cantidad que se quiere ingresar al parámetro saldo
+     * @throws Exception para no ingresar cantidades negativas
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
-            throw new Exception("No se puede ingresar una cantidad negativa");   
+            throw new Exception("No se puede ingresar una cantidad negativa");
         }
         saldo = saldo + cantidad;
     }
 
+    /**
+     * @param cantidad se mete la cantidad que se quiere retirar al parámetro saldo
+     * @throws Exception para no retirar cantidades negativas o mayores que el saldo actual
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
@@ -31,6 +52,9 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
 
+    /**
+     * @return nos devuelve el atributo nombre
+     */
     public String getNombre() {
         return nombre;
     }
@@ -39,6 +63,9 @@ public class CCuenta {
         this.nombre = nombre;
     }
 
+    /**
+     * @return nos devuelve el atributo cuenta
+     */
     public String getCuenta() {
         return cuenta;
     }
@@ -47,6 +74,9 @@ public class CCuenta {
         this.cuenta = cuenta;
     }
 
+    /**
+     * @return nos devuelve el atributo saldo
+     */
     public double getSaldo() {
         return saldo;
     }
@@ -55,6 +85,9 @@ public class CCuenta {
         this.saldo = saldo;
     }
 
+    /**
+     * @return nos devuelve el atributo tipoInteres
+     */
     public double getTipoInteres() {
         return tipoInteres;
     }
